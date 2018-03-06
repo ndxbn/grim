acdfcc5e7563c8a1ff2bcd86aa3fe78317b760ab
ndxbn <sitri.ndxbn@gmail.com>
2018-03-06 20:07:54 +0900

    systemctl enable should exec aflter start

--- script/install.sh
+++ script/install.sh
@@ -26,5 +26,5 @@ yum install -y \
   lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y docker-ce
-systemctl enable docker
-systemctl start docker
+systemctl start docker && systemctl enable docker
+
